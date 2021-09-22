ef65cfd7ec0e451d47328644966f31733095275b
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-21 18:09:20 -0600

    set up basic routes for search and display area

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,10 @@
 import { Route } from 'react-router-dom';
 import './App.css';
+import Search from '../Search/Search';
+import DisplayArea from '../DisplayArea/DisplayArea';
 
 const App = () => {
-  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +13,14 @@ const App = () => {
         </h1>
       </header>
       <main>
-
+        <Route exact path="/" render={ ({ match }) => {
+          return <Search />
+          }
+        }/>
+        <Route exact path="/:recipeID" render={ ({ match }) => {
+          return <DisplayArea />
+          }
+        }/>
       </main>
     </div>
   );
