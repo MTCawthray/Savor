591e7c0050d043182106fa73fe5be93bb351a33f
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-23 17:28:33 -0600

    Create basic card layout

--- src/Components/DisplayArea/DisplayArea.js
+++ src/Components/DisplayArea/DisplayArea.js
@@ -2,9 +2,24 @@ import './DisplayArea.css';
 
 const DisplayArea = ({ recipes }) => {
 
+  const cards = recipes.map(recipe => {
+    console.log(recipe)
+    return (
+      <article className="recipe-card">
+        <a href={recipe.recipe.url}>
+          <div className="name-and-img-container">
+            <h2>{recipe.recipe.label}</h2>
+            <img src={recipe.recipe.image} alt={recipe.recipe.label} className="card-img"/>
+          </div>
+        </a>
+        <h3>cuisine: {recipe.recipe.cuisineType}</h3>
+        <h3>calories: {recipe.recipe.calories}</h3>
+      </article>
+    )
+  })
   return (
     <section className="display-section">
-      hello from the display area!
+      {cards}
     </section>
   )
 }
