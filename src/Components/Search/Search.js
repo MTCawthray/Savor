c2c76c4bcbb634897dd1fc1d2136e48f5dd26079
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-23 15:45:28 -0600

    Add link tag to submit button and set props

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,16 +1,17 @@
 import './Search.css';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import apiCall from '../../utilities';
 
-const Search = () => {
-  const [recipes, setRecipes] = useState([]);
+const Search = (props) => {
+  // const [recipes, setRecipes] = useState([]);
   const [error, setError] = useState('');
   const [search, setSearch] = useState('');
 
   const submitSearch = (e) => {
     e.preventDefault();
     apiCall.getRecipes(search)
-      .then((data) => setRecipes(data.hits))
+      .then((data) => props.setRecipes(data.hits))
       .catch((err) => setError(err));
     setSearch('');
   }
@@ -24,10 +25,12 @@ const Search = () => {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <button
-      onClick={submitSearch}
-      >Search Recipes
-      </button>
+      <Link to="/recipes" >
+        <button
+        onClick={submitSearch}
+        >Search Recipes
+        </button>
+      </Link>
     </form>
   )
 }
