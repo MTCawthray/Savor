1be7978908156a91b3265a9cd6b2ce41c0e61a5a
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-23 09:08:12 -0600

    Add error check in utilities to handle api response

--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,16 @@
 require('dotenv').config();
 
 //this should be grabbing my api key from the ignored .env directory
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.REACT_APP_API_KEY;
+export const getRecipes = (type) => {
+  fetch(`https://api.spoonacular.com/recipes/?query=${type}&number=5&${apiKey}`)
+  .then(response => checkForErrors(response))
+}
+
+const checkForErrors = (res) => {
+  if (res.ok) {
+    return response.json();
+  } else {
+    throw `${response.status} ERROR. Could not access server data.`
+  }
+}
