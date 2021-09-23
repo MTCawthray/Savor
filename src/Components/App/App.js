3d1124bc521aaef905c3b7e721039d98029ce72e
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-23 17:28:50 -0600

    Refactor conditional in home route

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,16 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import React, { useState } from 'react';
 import './App.css';
 import Search from '../Search/Search';
 import DisplayArea from '../DisplayArea/DisplayArea';
-require('dotenv').config();
+// require('dotenv').config();
 
 const App = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  const addRecipes = (data) => {
+    setRecipes(data)
+  }
 
   return (
     <div className="App">
@@ -14,8 +20,20 @@ const App = () => {
         </h1>
       </header>
       <main>
-        <Route exact path="/" component={Search}/>
-        <Route exact path="/recipes" component={DisplayArea}/>
+        <Switch>
+          <Route exact path="/">
+          {recipes.length && <Redirect to="/recipes" />}
+          <Search 
+            addRecipes={addRecipes}
+          />
+          </Route>
+          <Route exact path="/recipes" render={ () => {
+            return <DisplayArea 
+              recipes={recipes}
+            />
+          }}
+          />
+        </Switch> 
       </main>
     </div>
   );
