a7fd0205867c13ec73f1ae821c319d1c7669dfbe
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-25 14:02:35 -0600

    Add alignment and style to the favorites toggle

--- src/Components/DisplayArea/DisplayArea.js
+++ src/Components/DisplayArea/DisplayArea.js
@@ -31,10 +31,26 @@ const DisplayArea = ({ favorites, recipes, handleFavorites }) => {
             <h2>{recipe.recipe.label}</h2>
           </div>
         </a>
-        <div className="detail-container">
-          <h3>cuisine: {recipe.recipe.cuisineType}</h3>
-          <h3>calories: {recipe.recipe.calories.toFixed()}</h3>
-          <h3>makes {recipe.recipe.yield} servings</h3>
+        <div className="card-bottom">
+          <div className="detail-container">
+            <h3>cuisine: {recipe.recipe.cuisineType}</h3>
+            <h3>calories: {recipe.recipe.calories.toFixed()}</h3>
+            <h3>makes {recipe.recipe.yield} servings</h3>
+          </div>
+          {favorites.includes(recipe) && (
+          <img 
+            src={selected} 
+            alt="in favorites" 
+            className="favorite-toggle" 
+            onClick={toggleFavorite} />
+        )}
+        {!favorites.includes(recipe) && (
+          <img 
+            src={unselected} 
+            alt="add to favorites" 
+            className="favorite-toggle" 
+            onClick={toggleFavorite} />
+        )}
         </div>
       </article>
         
