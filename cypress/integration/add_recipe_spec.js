193bd6b99bce8ceb02556e1171ac530ad0ceca9d
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-25 19:37:13 -0600

    Write a before each that intercepts and stubs

--- cypress/integration/add_recipe_spec.js
+++ cypress/integration/add_recipe_spec.js
@@ -1,3 +1,26 @@
 describe('Adding recipes to My Recipes section', ()=> {
-  
+
+  beforeEach(() => {
+    cy.intercept('GET', '/recipes', {
+      statusCode: 200,
+      body: {
+        "hits": [
+          {
+          "recipe": {
+              "label": "Carrot Limeade",
+              "image": "https://www.edamam.com/web-img/fc8/fc8e6d4ba4b0e0ea34aec2d54110e03a",
+              "source": "Martha Stewart",
+              "url": "https://www.marthastewart.com/1547130/carrot-limeade",
+              "yield": 8.0,
+              "calories": 356.8119479975469,
+              "cuisineType": ["american"]
+              }
+            }
+          ]
+        }
+      } 
+    )
+    cy.visit('http://localhost:3000')
+  })
+
 })
