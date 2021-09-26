1ea9b555ecf05f8bcffdc4270d2f6302ac4da072
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-26 09:55:35 -0600

    Add fetch error prop to return

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -1,10 +1,11 @@
 import './ErrorPage.css';
 
-const ErrorPage = ({ message }) => {
+const ErrorPage = ({ message, fetchError }) => {
 
   return (
     <div className="error-display">
       <h4>{message}</h4>
+      <h4>{fetchError}</h4>
     </div>
   )
 }
