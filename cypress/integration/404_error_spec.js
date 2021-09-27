9173367462f8a016a1d157636ef283a1e567a30f
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-27 17:57:15 -0600

    Move intercept to second visit

--- cypress/integration/404_error_spec.js
+++ cypress/integration/404_error_spec.js
@@ -2,21 +2,19 @@ require('dotenv').config();
 
 const apiKey = process.env.REACT_APP_API_KEY;
 const appID = process.env.REACT_APP_ID;
-const url = `https://api.edamam.com/api/recipes/v2?q=hotdog&app_id=${appID}&app_key=${apiKey}&type=public`;
+const url = `https://api.edamam.com/api/recipes/v2?q=hot_dogs&app_id=${appID}&app_key=${apiKey}&type=publicbadurl`;
 
 describe('Error handling for 404 error', () => {
-  beforeEach(() => {
-    // cy.intercept('GET', `http://localhost:3000/recipes`, {statusCode: 400, body: {}} )
-    cy.visit('http://localhost:3000')
-    
-  });
 
   it('As a user, I should see a message that displays a 400 level error from the fetch response.', () => {
-    cy.intercept('GET', url, {statusCode: 400, body: {}} )
+    cy.visit('http://localhost:3000')
     cy.get('.search-bar')
     .type('hot dogs')
     .get('.submit-btn')
     .click()
+    cy.intercept('GET', url, {statusCode: 400} )
+    cy.visit('http://localhost:3000')
+   
   })
 
 });
