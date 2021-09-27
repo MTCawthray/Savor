f24a36487be92ebdff03d31a4b32194ec6876aa8
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-27 16:46:50 -0600

    Move intercept to it block

--- cypress/integration/404_error_spec.js
+++ cypress/integration/404_error_spec.js
@@ -1,22 +1,18 @@
+require('dotenv').config();
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const appID = process.env.REACT_APP_ID;
+const url = `https://api.edamam.com/api/recipes/v2?q=hotdog&app_id=${appID}&app_key=${apiKey}&type=public`;
+
 describe('Error handling for 404 error', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'http://localhost:3000/recipes', {
-      statusCode: 404,
-      body: [
-        {
-          "errorCode": "404",
-          "message": "Request not recognized",
-          "params": [
-            "failure"
-          ]
-        }
-      ]
-    })
+    // cy.intercept('GET', `http://localhost:3000/recipes`, {statusCode: 400, body: {}} )
     cy.visit('http://localhost:3000')
     
   });
 
   it('As a user, I should see a message that displays a 400 level error from the fetch response.', () => {
+    cy.intercept('GET', url, {statusCode: 400, body: {}} )
     cy.get('.search-bar')
     .type('hot dogs')
     .get('.submit-btn')
