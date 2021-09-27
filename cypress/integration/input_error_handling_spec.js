52eb86c2d299888ce8c2fed06683ccce8c9e13d6
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-27 12:33:55 -0600

    Add it block testing for no query warning

--- cypress/integration/input_error_handling_spec.js
+++ cypress/integration/input_error_handling_spec.js
@@ -5,7 +5,10 @@ describe('Error handling for search input', () => {
   }); 
 
   it('As a user, I should see an error message if I do not type a query before searching', () => {
-
+    cy.get('.submit-btn')
+      .click();
+      cy.get('.error-display')
+      .contains('You must enter a recipe query before you submit')
   });
 
 });
