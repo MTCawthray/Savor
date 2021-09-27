fab93bcc6d445ac804ea69ca50816731781f985e
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-27 12:19:08 -0600

    Add a before each to visit the site

---
+++ cypress/integration/input_error_handling_spec.js
@@ -0,0 +1,11 @@
+describe('Error handling for search input', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  }); 
+
+  it('As a user, I should see an error message if I do not type a query before searching', () => {
+
+  });
+
+});
