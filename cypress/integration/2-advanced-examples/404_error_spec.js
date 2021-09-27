0b501b1ab388bb3960159c8f7eec5c77cb245f02
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-27 14:29:59 -0600

    Update url in intercept

--- cypress/integration/2-advanced-examples/404_error_spec.js
+++ cypress/integration/2-advanced-examples/404_error_spec.js
@@ -1,6 +1,6 @@
 describe('Error handling for 404 error', () => {
   beforeEach(() => {
-    cy.intercept('GET', '/recipes', {
+    cy.intercept('GET', 'http://localhost:3000/recipes', {
       statusCode: 404,
       body: [
         {
@@ -13,14 +13,14 @@ describe('Error handling for 404 error', () => {
       ]
     })
     cy.visit('http://localhost:3000')
-    cy.get('.search-bar')
-      .type('hot dogs')
-      .get('.submit-btn')
-      .click()
+    
   });
 
   it('As a user, I should see a message that displays a 400 level error from the fetch response.', () => {
-
+    cy.get('.search-bar')
+    .type('hot dogs')
+    .get('.submit-btn')
+    .click()
   })
 
 });
